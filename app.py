01dfa560bd5f96e10a25597ac74bb1a31caac643
Ragib Mondal <124962194+ragibmondal@users.noreply.github.com>
2023-12-27 21:35:20 +0600

    Update app.py

--- app.py
+++ app.py
@@ -1,47 +1,42 @@
 from dotenv import load_dotenv
-
-load_dotenv()  # take environment variables from .env.
+load_dotenv() ## loading all the environment variables
 
 import streamlit as st
 import os
-import pathlib
-import textwrap
-
 import google.generativeai as genai
 
-from IPython.display import display
-from IPython.display import Markdown
-
-
-def to_markdown(text):
-  text = text.replace('•', '  *')
-  return Markdown(textwrap.indent(text, '> ', predicate=lambda _: True))
-
-os.getenv("GOOGLE_API_KEY")
 genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
 
-## Function to load OpenAI model and get respones
-
+## function to load Gemini Pro model and get repsonses
+model=genai.GenerativeModel("gemini-pro") 
+chat = model.start_chat(history=[])
 def get_gemini_response(question):
-    model = genai.GenerativeModel('gemini-pro')
-    response = model.generate_content(question)
-    return response.text
+    
+    response=chat.send_message(question,stream=True)
+    return response
 
 ##initialize our streamlit app
 
 st.set_page_config(page_title="Q&A Demo")
 
-st.header("Gemini Application")
+st.header("Gemini LLM Application")
+
+# Initialize session state for chat history if it doesn't exist
+if 'chat_history' not in st.session_state:
+    st.session_state['chat_history'] = []
 
 input=st.text_input("Input: ",key="input")
-
-
 submit=st.button("Ask the question")
 
-## If ask button is clicked
-
-if submit:
-    
+if submit and input:
     response=get_gemini_response(input)
+    # Add user query and response to session state chat history
+    st.session_state['chat_history'].append(("You", input))
     st.subheader("The Response is")
-    st.write(response)
+    for chunk in response:
+        st.write(chunk.text)
+        st.session_state['chat_history'].append(("Bot", chunk.text))
+st.subheader("The Chat History is")
+    
+for role, text in st.session_state['chat_history']:
+    st.write(f"{role}: {text}")
