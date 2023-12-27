713952c6aee61cac7ab7c35c39ba7ddb25c30389
Ragib Mondal <124962194+ragibmondal@users.noreply.github.com>
2023-12-27 22:23:29 +0600

    Update app.py

--- app.py
+++ app.py
@@ -13,16 +13,19 @@ from IPython.display import display
 from IPython.display import Markdown
 
 
+def to_markdown(text):
+  text = text.replace('•', '  *')
+  return Markdown(textwrap.indent(text, '> ', predicate=lambda _: True))
+
 os.getenv("GOOGLE_API_KEY")
 genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
 
 ## Function to load OpenAI model and get respones
-model = genai.GenerativeModel('gemini-pro')
-chat = model.start_chat(history=[])
+
 def get_gemini_response(question):
-    
-    response =chat.send_message(question,stream=True)
-    return response
+    model = genai.GenerativeModel('gemini-pro')
+    response = model.generate_content(question)
+    return response.text
 
 ##initialize our streamlit app
 
@@ -41,8 +44,4 @@ if submit:
     
     response=get_gemini_response(input)
     st.subheader("The Response is")
-    for chunk in response:
-        print(st.write(chunk.text))
-        print("_"*80)
-    
-    st.write(chat.history)
+    st.write(response)
